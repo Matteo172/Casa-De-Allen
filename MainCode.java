e01dc1c633138a97b552fc60f7167be3fe1385fc
John Matthew Malabag <jm.malabag05@gmail.com>
2025-11-16 11:51:55 +0800

    MainCode.java

--- MainCode.java
+++ MainCode.java
@@ -11,60 +11,228 @@ public class MainCode {
     }
 
     public static void loadingscreen(){
-        System.out.print("Loading");
+        System.out.println("                     ");
+        System.out.println("|   _  _  _|. _  _  ");
+        System.out.println("|__(_)(_|(_||| )(_) ");
+        System.out.print("                _/  ");
         try{
             Thread.sleep(800);
         }catch (InterruptedException e){
-            } System.out.print(".");
+            } System.out.print(" ▄ ");
         try{
             Thread.sleep(800);
         }catch (InterruptedException e){
-            } System.out.print(".");
+            } System.out.print(" ▄ ");
         try{
             Thread.sleep(800);
         }catch (InterruptedException e){
-            } System.out.print(".");
+            } System.out.print(" ▄ ");
+        try{
+            Thread.sleep(800);
+        }catch (InterruptedException e){
+            } System.out.print(" ▄ ");
     }
+
+    public static void loadingscreen2(){
+            System.out.println("                     ");
+            System.out.println("|   _  _  _|. _  _  ");
+            System.out.println("|__(_)(_|(_||| )(_) ");
+            System.out.print("                _/  ");
+            try{
+                Thread.sleep(400);
+            }catch (InterruptedException e){
+                } System.out.print(" ▄ ");
+            try{
+                Thread.sleep(400);
+            }catch (InterruptedException e){
+                } System.out.print(" ▄ ");
+            try{
+                Thread.sleep(400);
+            }catch (InterruptedException e){
+                } System.out.print(" ▄ ");
+            try{
+                Thread.sleep(400);
+            }catch (InterruptedException e){
+                } System.out.print(" ▄ ");
+        }
 
     public static void lougoutscreen(){
-        System.out.print("Log-outing");
+
+        System.out.println("                             ");
+        System.out.println("|   _  _  __  _    |_. _  _  ");
+        System.out.println("|__(_)(_)    (_)|_||_|| )(_) ");
+        System.out.print("      _/                 _/  ");
         try{
             Thread.sleep(800);
         }catch (InterruptedException e){
-            } System.out.print(".");
+            } System.out.print(" ▄ ");
         try{
             Thread.sleep(800);
         }catch (InterruptedException e){
-            } System.out.print(".");
+            } System.out.print(" ▄ ");
         try{
             Thread.sleep(800);
         }catch (InterruptedException e){
-            } System.out.print(".");
+            } System.out.print(" ▄ ");
+        try{
+            Thread.sleep(800);
+        }catch (InterruptedException e){
+            } System.out.print(" ▄ ");
     }
 
-    static String type;
+    public static void checkingdatabasescreen(){
+
+        System.out.println("\n __                   __                    ");
+        System.out.println("/  |_  _ _| . _  _   |  \\ _ |_ _ |_  _  _ _ ");
+        System.out.println("\\__| )(-(_|(|| )(_)  |__/(_||_(_||_)(_|_)(-_ ");
+        System.out.print("                _/                          ");
+        try{
+            Thread.sleep(800);
+        }catch (InterruptedException e){
+            } System.out.print(" ▄ ");
+        try{
+            Thread.sleep(800);
+        }catch (InterruptedException e){
+            } System.out.print(" ▄ ");
+        try{
+            Thread.sleep(800);
+        }catch (InterruptedException e){
+            } System.out.print(" ▄ ");
+        try{
+            Thread.sleep(800);
+        }catch (InterruptedException e){
+            } System.out.print(" ▄ ");
+    }
+
+    public static void verifyingscreen(){
+
+        System.out.println("         _             __  ");
+        System.out.println("\\  /_ _.(_  . _  _   ||  \\ ");
+        System.out.println(" \\/(-| || \\/|| )(_)  ||__/ ");
+        System.out.print("          /     _/          ");
+        try{
+            Thread.sleep(400);
+        }catch (InterruptedException e){
+            } System.out.print(" ▄ ");
+        try{
+            Thread.sleep(400);
+        }catch (InterruptedException e){
+            } System.out.print(" ▄ ");
+        try{
+            Thread.sleep(400);
+        }catch (InterruptedException e){
+            } System.out.print(" ▄ ");
+        try{
+            Thread.sleep(400);
+        }catch (InterruptedException e){
+            } System.out.print(" ▄ ");
+    }
+
+    public static void Exitscreen(){
+
+        System.out.println(" __             ");
+        System.out.println("|_   .|_. _  _  ");
+        System.out.println("|__)(||_|| )(_) ");
+        System.out.print("            _/  ");
+        try{
+            Thread.sleep(800);
+        }catch (InterruptedException e){
+            } System.out.print(" ▄ ");
+        try{
+            Thread.sleep(800);
+        }catch (InterruptedException e){
+            } System.out.print(" ▄ ");
+        try{
+            Thread.sleep(800);
+        }catch (InterruptedException e){
+            } System.out.print(" ▄ ");
+        try{
+            Thread.sleep(800);
+        }catch (InterruptedException e){
+            } System.out.print(" ▄ ");
+    }
+
+    public static void Alynere(){
+        System.out.println("                                                                                                     ");
+        System.out.println("                                                                                                                   ▄▄ ");
+        System.out.println("             █████▄ ▄▄ ▄▄▄▄▄ ▄▄  ▄▄ ▄▄ ▄▄ ▄▄▄▄▄ ▄▄  ▄▄ ▄▄ ▄▄▄▄   ▄▄▄    ▄████▄ ▄▄  ▄▄ ▄▄ ▄▄  ▄▄ ▄▄▄▄▄ ▄▄▄▄  ▄▄▄▄▄  ██ ");
+        System.out.println("             ██▄▄██ ██ ██▄▄  ███▄██ ██▄██ ██▄▄  ███▄██ ██ ██▀██ ██▀██   ██▄▄██ ██  ▀███▀ ███▄██ ██▄▄  ██▄█▄ ██▄▄   ██ ");
+        System.out.println("             ██▄▄█▀ ██ ██▄▄▄ ██ ▀██  ▀█▀  ██▄▄▄ ██ ▀██ ██ ████▀ ██▀██   ██  ██ ██▄▄▄ █   ██ ▀██ ██▄▄▄ ██ ██ ██▄▄▄  ▄▄ ");
+        System.out.println("                                                                                                     ");
+        System.out.println("                                                                                                     ");
+        System.out.println("                                                                                                     ");
+
+    }
+
+    public static void Kim(){
+        System.out.println("                                                                                    ");
+        System.out.println("                                                                                                  ▄▄ ");
+        System.out.println("                    █████▄ ▄▄ ▄▄▄▄▄ ▄▄  ▄▄ ▄▄ ▄▄ ▄▄▄▄▄ ▄▄  ▄▄ ▄▄ ▄▄▄▄   ▄▄▄    ██ ▄█▀ ▄▄ ▄▄   ▄▄  ██ ");
+        System.out.println("                    ██▄▄██ ██ ██▄▄  ███▄██ ██▄██ ██▄▄  ███▄██ ██ ██▀██ ██▀██   ████   ██ ██▀▄▀██  ██ ");
+        System.out.println("                    ██▄▄█▀ ██ ██▄▄▄ ██ ▀██  ▀█▀  ██▄▄▄ ██ ▀██ ██ ████▀ ██▀██   ██ ▀█▄ ██ ██   ██  ▄▄ ");
+        System.out.println("                                                                                    ");
+        System.out.println("                                                                                    ");
+        System.out.println("                                                                                    ");
+    }
+
+    public static void Barcelos(){
+
+        System.out.println("                                                                                                                                    ");
+        System.out.println("                                                                                                                                          ▄▄ ");
+        System.out.println("          █████▄ ▄▄ ▄▄▄▄▄ ▄▄  ▄▄ ▄▄ ▄▄ ▄▄▄▄▄ ▄▄  ▄▄ ▄▄ ▄▄▄▄   ▄▄▄    ██▄  ▄██ ▄▄▄▄      █████▄  ▄▄▄  ▄▄▄▄   ▄▄▄▄ ▄▄▄▄▄ ▄▄     ▄▄▄   ▄▄▄▄  ██ ");
+        System.out.println("          ██▄▄██ ██ ██▄▄  ███▄██ ██▄██ ██▄▄  ███▄██ ██ ██▀██ ██▀██   ██ ▀▀ ██ ██▄█▄     ██▄▄██ ██▀██ ██▄█▄ ██▀▀▀ ██▄▄  ██    ██▀██ ███▄▄  ██ ");
+        System.out.println("          ██▄▄█▀ ██ ██▄▄▄ ██ ▀██  ▀█▀  ██▄▄▄ ██ ▀██ ██ ████▀ ██▀██   ██    ██ ██ ██ ▄   ██▄▄█▀ ██▀██ ██ ██ ▀████ ██▄▄▄ ██▄▄▄ ▀███▀ ▄▄██▀  ▄▄ ");
+        System.out.println("                                                                                                                                    ");
+
+    }
+
+    public static void welcomeuser(){
+
+        System.out.println("                                                                                ");
+        System.out.println("                                                                                      ▄▄ ");
+        System.out.println("          ██     ██ ▄▄▄▄▄ ▄▄     ▄▄▄▄  ▄▄▄  ▄▄   ▄▄ ▄▄▄▄▄   ██  ██  ▄▄▄▄ ▄▄▄▄▄ ▄▄▄▄   ██ ");
+        System.out.println("          ██ ▄█▄ ██ ██▄▄  ██    ██▀▀▀ ██▀██ ██▀▄▀██ ██▄▄    ██  ██ ███▄▄ ██▄▄  ██▄█▄  ██ ");
+        System.out.println("           ▀██▀██▀  ██▄▄▄ ██▄▄▄ ▀████ ▀███▀ ██   ██ ██▄▄▄   ▀████▀ ▄▄██▀ ██▄▄▄ ██ ██  ▄▄ ");
+        System.out.println("                                                                                ");
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         boolean checker = false;
-
-        System.out.println("  ____          _          _        _       _          _       ");
-        System.out.println(" / ___|   __ _ | |__   ___| | _____| | __ _| |__   ___| |_ ___ ");
-        System.out.println("| |      / _` || '_ \\ / _ \\ |/ / _ \\ |/ _` | '_ \\ / _ \\ __/ __|");
-        System.out.println("| |___  | (_| || | | |  __/   <  __/ | (_| | | | |  __/ |_\\__ \\");
-        System.out.println(" \\____|  \\__,_||_| |_|\\___|_|\\_\\___|_|\\__, |_| |_|\\___|\\__|___/");
-        System.out.println("                                     |___/                     ");
-
         
         clearscreen();
+
+
+        System.out.println("                                                                                                                              ");
+        System.out.println("                                                                                                                              ");
+        System.out.println("                                █████▄ ▄▄ ▄▄▄▄▄ ▄▄  ▄▄ ▄▄ ▄▄ ▄▄▄▄▄ ▄▄  ▄▄ ▄▄ ▄▄▄▄   ▄▄▄   ▄▄▄▄     ▄▄▄                        ");
+        System.out.println("                                ██▄▄██ ██ ██▄▄  ███▄██ ██▄██ ██▄▄  ███▄██ ██ ██▀██ ██▀██ ███▄▄    ██▀██                       ");
+        System.out.println("                                ██▄▄█▀ ██ ██▄▄▄ ██ ▀██  ▀█▀  ██▄▄▄ ██ ▀██ ██ ████▀ ▀███▀ ▄▄██▀    ██▀██                       ");
+        System.out.println("                                                                                                                              ");
+        System.out.println("                                                                                                                              ");
+        System.out.println("               ░██████                                      ░███████                    ░███    ░██ ░██                       ");
+        System.out.println("              ░██   ░██                                     ░██   ░██                  ░██░██   ░██ ░██                       ");
+        System.out.println("             ░██         ░██████    ░███████   ░██████      ░██    ░██  ░███████      ░██  ░██  ░██ ░██  ░███████  ░████████  ");
+        System.out.println("             ░██              ░██  ░██              ░██     ░██    ░██ ░██    ░██    ░█████████ ░██ ░██ ░██    ░██ ░██    ░██ ");
+        System.out.println("             ░██         ░███████   ░███████   ░███████     ░██    ░██ ░█████████    ░██    ░██ ░██ ░██ ░█████████ ░██    ░██ ");
+        System.out.println("              ░██   ░██ ░██   ░██         ░██ ░██   ░██     ░██   ░██  ░██           ░██    ░██ ░██ ░██ ░██        ░██    ░██ ");
+        System.out.println("               ░██████   ░█████░██  ░███████   ░█████░██    ░███████    ░███████     ░██    ░██ ░██ ░██  ░███████  ░██    ░██ ");
+        System.out.println("                                                                                                                              ");
+        System.out.println("                                                                                                                              ");
+
+        
         while(!checker){
-            System.out.println("Sign in as");
-            System.out.println("1. Client");
-            System.out.println("2. Receptionist");
-            System.out.println("3. Manager");
-            System.out.println("4. Exit");
+            System.out.println("╔══════════════════════════════════════════════════╗");
+            System.out.println("║                USER ACCESS MENU                  ║");
+            System.out.println("╠══════════════════════════════════════════════════╣");
+            System.out.println("║   1. Client                                      ║");
+            System.out.println("║   2. Receptionist                                ║");
+            System.out.println("║   3. Manager                                     ║");
+            System.out.println("║   4. Exit                                        ║");
+            System.out.println("╚══════════════════════════════════════════════════╝");
         
             try{
-            System.out.println("\nEnter your choice: ");
+            System.out.print("\nEnter your choice: ");
             int choice = scanner.nextInt();
             
                 switch(choice){
@@ -75,9 +243,21 @@ public class MainCode {
                     case 3: Manager.Manager(args);
                     break;
                     case 4: clearscreen();
-                            loadingscreen();
+                            Exitscreen();
                             clearscreen();
-                            System.out.println("Thank You. Come Again!!!");
+                            System.out.println("                                                                        ");
+                            System.out.println("                                                                        ");
+                            System.out.println("                     ▄████  ▄▄▄▄   ▄▄▄   ▄▄▄▄ ▄▄  ▄▄▄   ▄▄▄▄  ▄▄        ");
+                            System.out.println("                    ██  ▄▄▄ ██▄█▄ ██▀██ ██▀▀▀ ██ ██▀██ ███▄▄  ██        ");
+                            System.out.println("                     ▀███▀  ██ ██ ██▀██ ▀████ ██ ██▀██ ▄▄██▀  ██        ");
+                            System.out.println("                                                              ▄▄        ");
+                            System.out.println("                                                                        ");
+                            System.out.println("              ▄█████  ▄▄▄  ▄▄   ▄▄ ▄▄▄▄▄   ▄████▄  ▄▄▄▄  ▄▄▄  ▄▄ ▄▄  ▄▄ ");
+                            System.out.println("              ██     ██▀██ ██▀▄▀██ ██▄▄    ██▄▄██ ██ ▄▄ ██▀██ ██ ███▄██ ");
+                            System.out.println("              ▀█████ ▀███▀ ██   ██ ██▄▄▄   ██  ██ ▀███▀ ██▀██ ██ ██ ▀██ ");
+                            System.out.println("                                                                        ");
+                            System.out.println("                                                                        ");
+                            System.out.println("                                                                        ");
                             System.exit(0);
                     default:
                         System.out.println("Invalid Choice. Please Try again");
